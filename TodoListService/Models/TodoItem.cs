c50b6268fa466730f856a6dfc3d3362fee61fd07
gnanendragithub <mgnanendrareddy09@gmail.com>
2022-06-12 21:51:12 +0530

    hai

---
+++ TodoListService/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TodoListService.Models;
+
+public class Todo
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("owner")]
+    public string Owner { get; set; }
+}
